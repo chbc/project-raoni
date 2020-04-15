ed575e969612a7fe8c5fda2b20bc1df8c6135a00
Henrique <henrique.dsc@gmail.com>
2020-04-15 01:43:02 -0300

    Added parallax effect for bridge zone

---
+++ Assets/Scripts/Core/ParallaxController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace ProjectRaoni
+{
+    public class ParallaxController : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraTransform = null;
+
+        private Transform objectTransform;
+        private Vector3 initialPosition;
+        private Vector3 currentPosition;
+        private float zDistance;
+
+        private void Start()
+        {
+            this.objectTransform = base.transform;
+            this.initialPosition = this.objectTransform.position;
+            this.currentPosition = this.initialPosition;
+            this.zDistance = this.initialPosition.z - this.cameraTransform.position.z;
+        }
+
+        private void Update()
+        {
+            this.currentPosition.x = this.GetPosition();
+            this.objectTransform.position = this.currentPosition;
+        }
+
+        /*
+         * x = (n/Pz) * Px
+         */
+        private float GetPosition()
+        {
+            Vector3 cameraPosition = this.cameraTransform.position;
+            float relativeXPosition = this.initialPosition.x - cameraPosition.x;
+            return (cameraPosition.x + (relativeXPosition / this.zDistance));
+        }
+    }
+}
