6d02cc32600b3a7dcaf3a35d9f23c377e99dccb0
Henrique <henrique.dsc@gmail.com>
2020-05-25 10:45:08 -0300

    Fixed cutscene.

--- Assets/Scripts/SceneManagement/CutsceneController.cs
+++ Assets/Scripts/SceneManagement/CutsceneController.cs
@@ -12,6 +12,8 @@ namespace ProjectRaoni
 
         private IEnumerator Start()
         {
+            yield return null;
+            
             this.videoPlayer.Play();
 
             yield return null;
